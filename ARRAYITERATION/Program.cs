de976ae28db4439167164c18f6c3cfe7fdfa38b5
hidytech-a11y <hidytech@mail.com>
2025-11-11 13:36:04 +0100

    Initial commit - ARRAYITERATION

---
+++ ARRAYITERATION/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class ArrayIteration
+{
+
+    public static void Main(string[] args)
+    {
+        var colors = new[] { "Red", "Green", "Blue", "Yellow" };
+
+        Console.WriteLine("--- Using a for loop ---");
+
+        for (int i = 0; i < colors.Length; i++)
+
+        {
+            Console.WriteLine($"Index {i} {colors[i]}");
+
+        }
+
+        Console.WriteLine("\n--- Using a foreach loop ---");
+
+        foreach (var color in colors)
+        {
+            Console.WriteLine("Color: " + color);
+        }
+
+    }
+
+}
